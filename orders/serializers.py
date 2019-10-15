6cf779c7d21deb0425b726ba4755b41424596d34
Caleb Mah <calebmah@gmail.com>
2019-10-16 00:32:31 +0800

    Add validation for serializer

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 
-from .models import Orders
+from .models import MenuItems, Orders
 
 
 class OrderSerializer(serializers.ModelSerializer):
+
+    def validate(self, data):
+        """
+        Check that item is sold by restaurant
+        """
+        if data['item'].restaurant != data['restaurant']:
+            raise serializers.ValidationError(f"{data['restaurant']} does not sell {data['item']}")
+        return data
+
     class Meta:
         model = Orders
-        fields = ['id', 'created', 'user', 'restaurant', 'quantity','item','comments','total_price']
+        fields = ['id', 'created', 'user', 'restaurant', 'quantity', 'item', 'comments', 'total_price']
